8c2cf1b38f4a1a097b4e6e2b896196d8c7aa74ed
LLHOYH <llhoyh01@gmail.com>
2024-03-10 16:40:24 +1100

    Update lambda_function.py

--- lambda_function.py
+++ lambda_function.py
@@ -8,7 +8,7 @@ from botocore.awsrequest import AWSRequest
 import base64
 
 # grab environment variables
-ENDPOINT_NAME = "jumpstart-dft-efficientnet-lite1-fe-20240302-131428"
+ENDPOINT_NAME = os.environ["SageMakerEndPoint"]
 client= boto3.client('runtime.sagemaker')
 region = boto3.Session().region_name
 s3_bucket = f"jumpstart-cache-prod-{region}"
@@ -19,13 +19,24 @@ s3 = boto3.client("s3")
 def lambda_handler(event, context):
     
     
-    payload = str(event["body"])
-    result = base64.b64decode(payload)
-    print(result)
-    print(type(result))
+    payload = base64.b64decode(event["body"])
 
-    queryResponse = client.invoke_endpoint(EndpointName=ENDPOINT_NAME, ContentType='application/x-image', Body=result)
-    
+    queryResponse = client.invoke_endpoint(EndpointName=ENDPOINT_NAME, ContentType="application/x-image", Body=payload)
     model_predictions = json.loads(queryResponse['Body'].read())
     embedding = model_predictions['embedding']
-    return embedding
+    
+    statusCode=500
+    headers = {"Content-Type": "application/json"}
+    body={}
+    if embedding:
+        statusCode = 200
+        body=json.dumps({
+            "embedding":embedding
+        })
+        
+    response = {
+        "statusCode":statusCode,
+        "headers":headers,
+        "body":body
+    }
+    return response
